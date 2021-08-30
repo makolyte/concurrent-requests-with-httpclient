689ae64baff4730cde5b1b6590bb9aa6ebcc5b7f
Maclain Wiltzer <mwiltzer@gmail.com>
2021-08-30 15:22:55 -0400

    Added example console app that uses an HttpClient to make concurrent requests to the service stub in /server/

---
+++ client/ConcurrentRequestsExampleClient/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace ConcurrentRequestsExampleClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            var randoService = new RandomNumberService(url: "https://localhost:12345", maxConcurrentRequests: 4,
+                timeoutSeconds: 5);
+
+            for (int i = 0; i < 15; i++)
+            {
+                Task.Run(async () =>
+                {
+                    Console.WriteLine($"Requesting random number ");
+                    Console.WriteLine(await randoService.GetRandomNumber());
+                });
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ client/ConcurrentRequestsExampleClient/RandomNumberService.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Net.Sockets;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConcurrentRequestsExampleClient
+{
+    public class RandomNumberService
+	{
+		private readonly HttpClient HttpClient;
+		private readonly string GetRandomNumberUrl;
+		private SemaphoreSlim semaphore;
+		private long circuitStatus;
+		private const long OPEN = 0;
+		private const long TRIPPED = 1;
+		public string UNAVAILABLE = "Unavailable";
+
+		public RandomNumberService(string url, int maxConcurrentRequests, int timeoutSeconds)
+		{
+			GetRandomNumberUrl = $"{url}/RandomNumber/";
+
+			HttpClient = new HttpClient();
+			HttpClient.Timeout = TimeSpan.FromSeconds(timeoutSeconds);
+			SetMaxConcurrency(url, maxConcurrentRequests);
+			semaphore = new SemaphoreSlim(maxConcurrentRequests);
+
+			circuitStatus = OPEN;
+		}
+
+		private void SetMaxConcurrency(string url, int maxConcurrentRequests)
+		{
+			ServicePointManager.FindServicePoint(new Uri(url)).ConnectionLimit = maxConcurrentRequests;
+		}
+
+		public void OpenCircuit()
+		{
+			if (Interlocked.CompareExchange(ref circuitStatus, OPEN, TRIPPED) == TRIPPED)
+			{
+				Console.WriteLine("Opened circuit");
+			}
+		}
+		private void TripCircuit(string reason)
+		{
+			if (Interlocked.CompareExchange(ref circuitStatus, TRIPPED, OPEN) == OPEN)
+			{
+				Console.WriteLine($"Tripping circuit because: {reason}");
+			}
+		}
+		private bool IsTripped()
+		{
+			return Interlocked.Read(ref circuitStatus) == TRIPPED;
+		}
+		public async Task<string> GetRandomNumber()
+		{
+			try
+			{
+				await semaphore.WaitAsync();
+
+				if (IsTripped())
+				{
+					return UNAVAILABLE;
+				}
+
+				var response = await HttpClient.GetAsync(GetRandomNumberUrl);
+
+				if (response.StatusCode != HttpStatusCode.OK)
+				{
+					TripCircuit(reason: $"Status not OK. Status={response.StatusCode}");
+					return UNAVAILABLE;
+				}
+
+				return await response.Content.ReadAsStringAsync();
+			}
+			catch (Exception ex) when (ex is OperationCanceledException || ex is TaskCanceledException)
+			{
+				//Note: If you want to simulate timeouts to see this error handling path, I suggest using toxiproxy.
+				//Ref: https://makolyte.com/how-to-use-toxiproxy-to-verify-your-code-can-handle-timeouts-and-unavailable-endpoints/
+				Console.WriteLine("Timed out");
+				TripCircuit(reason: $"Timed out");
+				return UNAVAILABLE;
+			}
+			catch(HttpRequestException ex) when (ex?.InnerException is SocketException sockEx && sockEx.SocketErrorCode == SocketError.ConnectionRefused)
+            {
+				//Note: The simplest way to see the error handling in action is to not run the service. 
+				//That will result in this error happening, instead of timeout.
+				Console.WriteLine("Connection failed.");
+				TripCircuit(reason: $"Connection failed");
+				return UNAVAILABLE;
+            }
+			finally
+			{
+				semaphore.Release();
+			}
+		}
+	}
+}
